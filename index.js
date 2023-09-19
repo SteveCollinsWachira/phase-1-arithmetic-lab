5555ceb605501317a9b2a9fa05d013fac1c39086
Steve <wachirastevecollins@gmail.com>
2023-09-19 13:08:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1=10
+let num2=6.2
+let multiply =(num1*num2) 
+    
+let random = Math.ceil(Math.random()*10);
 
+let num3 = 24
+let num4 = 5 
+let mod =(num3 % num4)
+
+const max =Math.max (17,18,19,20);
 // Write your code here
